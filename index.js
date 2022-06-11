5499922f94f23f440bfdfe00195b637e763e8886
Florian2807 <gamerflo3.3@gmail.com>
2022-06-11 20:56:19 +0200

    change some stuff

--- index.js
+++ index.js
@@ -3,18 +3,49 @@ const got = require('got')
 const express = require('express')
 const app = express()
 
+let lastDowntime
+let allChannels = []
+
+checkIsDown()
+getAvaiableChannels()
+
 app.get('/api/v2/recent-messages/:channel/', (req, res) => {
+    const now = new Date().getTime()
+    const hoursBetweenDates = Math.abs(lastDowntime - now) / (60 * 60 * 1000)
     const channel = req.params.channel
     const limit = req.query.limit ?? 800
-    getLogs(channel, limit).then(i => {
-        const messages = i
-        console.log(messages.length)
-        res.send({
-            "error": null,
-            "error_code": null,
-            "messages": messages
+    if (hoursBetweenDates < 24) { // if the last downtime was less than 24 hours ago, we use JustLogs insances to get the messages
+        if (!allChannels.includes(channel)) {
+            res.send({
+                "error": "The bot is currently not joined to this channel (in progress or failed previously)",
+                "error_code": "channel_not_joined",
+                "messages": []
+            })
+        } else {
+            got(`https://recent-messages.florian2807.me/api/v2/recent-messages/${channel}`).json().then(response => {
+                let i = 0
+                let messageLimit = response.messages.length < limit ? response.messages.length : limit
+
+                response.messages //.slice(0, messageLimit)
+                console.log(response.messages.length)
+                console.log(messageLimit)
+                const recentMessages = response.messages.forEach(message => {
+
+                    message = parseIrcMessage(message)
+                    i++
+                })
+                res.send({
+                    "error": null,
+                    "error_code": null,
+                    "messages": response.messages
+                })
+            })
+        }
+    } else { // if the last downtime was more than 24 hours ago, we use the recentMessages API to get the messages
+        got(`https://recent-messages.robotty.de/api/v2/recent-messages/${channel}?limit=${limit}`).json().then(response => {
+            res.send(response)
         })
-    })
+    }
 })
 
 const server = http.createServer(app)
@@ -23,54 +54,37 @@ server.listen(5000, () => {
     console.log('listening on port 5000')
 })
 
-getLogs("florian_2807", 800)
-
-async function getLogs(channel, limit) {
-    const today = new Date()
-    const yesterday = new Date(today)
-
-    yesterday.setDate(yesterday.getDate() - 1)
-
-    today.toDateString()
-    yesterday.toDateString()
-
-    const days = [
-        {
-            "info": "today",
-            "day": today.getDate(),
-            "month": today.getMonth() + 1,
-            "year": today.getFullYear()
-        },
-        {
-            "info": "yesterday",
-            "day": yesterday.getDate(),
-            "month": yesterday.getMonth() + 1,
-            "year": yesterday.getFullYear()
-        }
-    ]
-    let justlogMessages = {"today": [], "yesterday": []}
-    let counter = 0
-    for (const i of days) {
-        try {
-            const {body} = await got(`https://logs.florian2807.me/channel/${channel}/${i.year}/${i.month}/${i.day}?reverse&json=1`)
-            for (let c = 0; counter < limit && c < JSON.parse(body).messages.length; c++) {
-                counter++
-                justlogMessages[i.info].push(parseIrcMessage(JSON.parse(body).messages[c].raw))
-            }
-        } catch {}
-
-    }
-    const messages = justlogMessages["today"].concat(justlogMessages["yesterday"])
-    return messages.slice(limit - limit * 2)
-}
 
 function parseIrcMessage(ircMsg) {
-    let regexTmiTS = /tmi-sent-ts=(\d+)/
-    let regexInsertRMTags = /(.+flags=;)(id=.+mod=\d;)(room-id=.+)/
+    try {
+        let regexTmiTS = /tmi-sent-ts=(\d+)/
+        let regexInsertRMTags = /(.+flags=;)(id=.+mod=\d;)(room-id=.+)/
 
-    let tmiTS = regexTmiTS.exec(ircMsg)[1]
+        let tmiTS = regexTmiTS.exec(ircMsg)[1]
 
-    let rmMsg = ircMsg.replace(regexInsertRMTags, `$1historical=1;$2rm-received-ts=${tmiTS};$3`)
+        let rmMsg = ircMsg.replace(regexInsertRMTags, `$1historical=1;$2rm-received-ts=${tmiTS};$3`)
 
-    return rmMsg
+        return rmMsg
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+setInterval(async () => {
+    await checkIsDown()
+    await getAvaiableChannels()
+}, 6000)
+
+async function checkIsDown() {
+    const {isDown} = await got("https://api-prod.downfor.cloud/httpcheck/https://recent-messages.robotty.de/api/v2/recent-messages/florian_2807").json()
+    if (isDown) {
+        console.log('xd')
+        lastDowntime = new Date().getTime()
+    }
+
+}
+
+async function getAvaiableChannels() {
+    const channels = await got("https://logs.florian2807.me/channels").json()
+    allChannels = channels
 }
