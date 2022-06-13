f862cb0d576af28d1dd3692fbe5a4582c94dcf69
Florian2807 <gamerflo3.3@gmail.com>
2022-06-13 09:53:49 +0200

    added case that recentmessages is blocked in the specified channel

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import http from 'http'
 import got from 'got'
-import express, { Express } from 'express';
+import express, {Express} from 'express';
 
-const app : Express = express()
+const app: Express = express()
 
 const config = require('./config.json')
 
-let lastRMDowntime : Date = new Date(0)
-let loggedChannels : string[] = []
+let lastRMDowntime: Date = new Date(0)
+let loggedChannels: string[] = []
 
 checkIsDown()
 getAvailableChannels()
@@ -34,7 +34,8 @@ app.get('/api/v2/recent-messages/:channel/', (request, response) => {
     const isLogged = loggedChannels.includes(requestedChannel)
     if (!isLogged || hoursSinceLastDowntime > 24) {
         const recentMessages = `${config.recentMsgInstance}/api/v2/recent-messages/${requestedChannel}?limit=${requestedLimit}`
-        got(recentMessages).then(result => {
+        got(recentMessages, {throwHttpErrors: false} ).then(result => {
+            console.log(result.rawBody)
             response.header('content-type', 'application/json')
             response.send(result.rawBody)
         }).catch(() => {
@@ -42,27 +43,27 @@ app.get('/api/v2/recent-messages/:channel/', (request, response) => {
             console.log('recent-messages request failed')
             response.sendStatus(500)
         })
-    }
-    else {
+    } else {
         got(`${config.recentMsgJustLogsInstance}/${requestedChannel}`).json<RecentMessages>().then(result => {
-            const messageLimit = Math.min(result.messages.length, requestedLimit)
-            result.messages = result.messages.slice(0, messageLimit)
 
-            const recentMessages : string[] = []
-            result.messages.forEach(message => {
-                recentMessages.push(convertIRCMessage(message))
-            })
+                const messageLimit = Math.min(result.messages.length, requestedLimit)
+                result.messages = result.messages.slice(0, messageLimit)
 
-            response.send({
-                "error": null,
-                "error_code": null,
-                "messages": recentMessages
-            })
+                const recentMessages: string[] = []
+                result.messages.forEach(message => {
+                    recentMessages.push(convertIRCMessage(message))
+                })
+
+                response.send({
+                    "error": null,
+                    "error_code": null,
+                    "messages": recentMessages
+                })
         }).catch(() => response.sendStatus(500))
     }
 })
 
-function convertIRCMessage(ircMsg : string) {
+function convertIRCMessage(ircMsg: string) {
     let regexTmiTS = /tmi-sent-ts=(\d+)/
     let regexInsertRMTags = /(.+flags=;)(id=.+mod=\d;)(room-id=.+)/
 
@@ -78,8 +79,8 @@ function checkIsDown() {
             lastRMDowntime = new Date()
         }
     }).catch(() => {
-            lastRMDowntime = new Date()
-        })
+        lastRMDowntime = new Date()
+    })
 }
 
 function getAvailableChannels() {
@@ -87,7 +88,7 @@ function getAvailableChannels() {
 }
 
 interface RecentMessages {
-    error : string
-    error_code : string
-    messages : string[]
+    error: string
+    error_code: string
+    messages: string[]
 }
