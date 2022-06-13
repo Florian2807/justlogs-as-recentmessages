b60650f6cc3b55d2818f5d5f829083a31a851be7
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2022-06-13 12:23:55 +0200

    refactor: clean up some stuff (#1)
    * chore: fix config.json indentation + no newline
    
    * refactor: remove unused request argument for /status/ API route
    
    * refactor: clean up loading of stored downtime
    
    * chore: clean up fs import
    
    * refactor: remove statuspage workaround
    
    * chore: fix spacing and move around a few things for more clarity

--- index.ts
+++ index.ts
@@ -1,32 +1,31 @@
 import http from 'http'
 import got from 'got'
 import express, {Express} from 'express';
-import * as fs from "fs";
-
-const app: Express = express()
+import fs from 'fs';
 
 const config = require('./config.json')
 
-const lastDowntime: string = fs.readFileSync('./last-down.json', 'utf8') || '1970-01-01T00:00:00.000Z'
-let lastRMDowntime: Date = new Date(lastDowntime)
+const lastRecordedRMDowntime : string = fs.readFileSync('./last-down.json', 'utf8')
+let lastRMDowntime: Date = new Date(lastRecordedRMDowntime || 0)
 let loggedChannels: string[] = []
+
 
 checkIsDown()
 getAvailableChannels()
-
 setInterval(() => {
     checkIsDown()
     getAvailableChannels()
 }, 60000)
 
-const server = http.createServer(app)
 
+const app: Express = express()
+const server = http.createServer(app)
 server.listen(1234, () => {
     console.log('listening on port 1234')
 })
 
 
-app.get('/status/', (request, response) => {
+app.get('/status/', (_, response) => {
     const timeSinceLastDowntime = Date.now() - lastRMDowntime.getTime()
     const hoursSinceLastDowntime = timeSinceLastDowntime / 1000 / 60 / 60
     response.send({
@@ -41,9 +40,7 @@ app.get('/api/v2/recent-messages/:channel/', (request, response) => {
 
     const requestedChannel = request.params.channel
     const requestedLimit = parseInt(request.query.limit as string) || 800
-    if (requestedChannel !== "statuspage") {
-        console.log("request for channel " + requestedChannel)
-    }
+
     const isLogged = loggedChannels.includes(requestedChannel)
     if (!isLogged || hoursSinceLastDowntime > 24) {
         requestRecentMSG(response, requestedChannel, requestedLimit)
@@ -52,8 +49,10 @@ app.get('/api/v2/recent-messages/:channel/', (request, response) => {
     }
 })
 
+
 function requestRecentMSG(response: any, requestedChannel: string, requestedLimit: number) {
     const recentMessages = `${config.recentMsgInstance}/api/v2/recent-messages/${requestedChannel}?limit=${requestedLimit}`
+
     got(recentMessages, {throwHttpErrors: false}).then(result => {
         response.header('content-type', 'application/json')
         if (response.error !== null) {
@@ -68,10 +67,8 @@ function requestRecentMSG(response: any, requestedChannel: string, requestedLimi
     })
 }
 
-
 function requestJustLogs(response: any, requestedChannel: string, requestedLimit: number) {
     got(`${config.recentMsgJustLogsInstance}/${requestedChannel}`).json<RecentMessages>().then(result => {
-
         const messageLimit = Math.min(result.messages.length, requestedLimit)
         result.messages = result.messages.slice(0, messageLimit)
 
@@ -94,7 +91,6 @@ function convertIRCMessage(ircMsg: string) {
     let regexInsertRMTags = /(.+flags=;)(id=.+mod=\d;)(room-id=.+)/
 
     let tmiTS = regexTmiTS.exec(ircMsg)?.[1]
-
 
     return ircMsg.replace(regexInsertRMTags, `$1historical=1;$2rm-received-ts=${tmiTS};$3`)
 }
