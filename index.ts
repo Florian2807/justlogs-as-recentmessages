fdfc7cb710d42dc8f8114837ca08c591072beafd
Florian2807 <gamerflo3.3@gmail.com>
2022-09-18 18:16:27 +0200

    added function to use multiple recent-messages instances.

--- index.ts
+++ index.ts
@@ -4,16 +4,21 @@ import express, {Express} from 'express';
 import fs from 'fs';
 
 const config = require('./config.json')
+const lastRecordedRMDowntime: { [key: string]: string | null } = require('./last-down.json')
 
-const lastRecordedRMDowntime: string = fs.readFileSync('./last-down.json', 'utf8')
-let lastRMDowntime: Date = new Date(Date.parse(lastRecordedRMDowntime) || 0)
+checkCorrectConfig();
+
+let lastRMDowntime: { [key: string]: Date } = {}
+Object.keys(lastRecordedRMDowntime).forEach(key => {
+    lastRMDowntime[key] = new Date(lastRecordedRMDowntime[key] || 0)
+})
 let loggedChannels: string[] = []
 
 
-checkIsDown()
+getAvailableRecentMSG()
 getAvailableChannels()
 setInterval(() => {
-    checkIsDown()
+    getAvailableRecentMSG()
     getAvailableChannels()
 }, 60000)
 
@@ -27,42 +32,54 @@ server.listen(config.port, () => {
 app.get('')
 
 app.get('/status/', (_, response) => {
-    const timeSinceLastDowntime = Date.now() - lastRMDowntime.getTime()
+    const timeSinceLastDowntime = Date.now() - lastRMDowntime["https://recent-messages.robotty.de"].getTime()
     const hoursSinceLastDowntime = timeSinceLastDowntime / 1000 / 60 / 60
     response.send({
-        "lastDowntime": lastRMDowntime.getTime() ? lastRMDowntime : null,
+        "lastDowntime": lastRMDowntime["https://recent-messages.robotty.de"].getTime() ? lastRMDowntime : null,
         "last24Hours": hoursSinceLastDowntime < 24,
     })
 })
 
 app.get('/api/v2/recent-messages/:channel/', (request, response) => {
 
-    const timeSinceLastDowntime = Date.now() - lastRMDowntime.getTime()
-    const hoursSinceLastDowntime = timeSinceLastDowntime / 1000 / 60 / 60
-
+    let instanceStatus: { [key: string]: boolean } = {}
+    let usefulInstance: string | null = ""
+    for (const instance of config.recentMsgInstance) {
+        const timeSinceLastDowntime = Date.now() - lastRMDowntime[instance]?.getTime() || 0
+        const hoursSinceLastDowntime = timeSinceLastDowntime / 1000 / 60 / 60
+        instanceStatus[instance] = hoursSinceLastDowntime < 24 // true means instance has downtime
+    }
+    console.log("instanceStatus: " + JSON.stringify(instanceStatus))
+    for (let i = Object.keys(instanceStatus)?.length; i > 0; i--) {
+        const instance = instanceStatus[Object.keys(instanceStatus)[i-1]]
+        console.log(instance)
+        if (!instance) {
+            usefulInstance = Object.keys(instanceStatus)[i-1]
+            console.log(usefulInstance)
+        }
+    }
+    console.log("usefulInstance: " + usefulInstance)
     const requestedChannel = request.params.channel
     const requestedLimit = parseInt(request.query.limit as string) || 800
-
     if (request.query.justlogs) {
         requestJustLogs(response, requestedChannel, requestedLimit)
     } else if (request.query.recentmsg) {
-        requestRecentMSG(response, requestedChannel, requestedLimit)
+        requestRecentMSG(response, requestedChannel, requestedLimit, usefulInstance)
     }
 
-
     const isLogged = loggedChannels.includes(requestedChannel)
-    if (!isLogged || hoursSinceLastDowntime > 24) {
-        console.log(`requesting recent messages for ${requestedChannel} NoLogs: ${!isLogged} wasDown: ${hoursSinceLastDowntime < 24}`)
-        requestRecentMSG(response, requestedChannel, requestedLimit)
+    if (!isLogged && usefulInstance.length > 0) {
+        console.log(`requesting ${usefulInstance} for ${requestedChannel} NoLogs: ${!isLogged} wasDown: ${instanceStatus[usefulInstance] ?? true}`)
+        requestRecentMSG(response, requestedChannel, requestedLimit, usefulInstance)
     } else {
-        console.log(`requesting JustLogs for ${requestedChannel} isLogged: ${!isLogged} wasDown: ${hoursSinceLastDowntime < 24}`)
+        console.log(`requesting JustLogs for ${requestedChannel} isLogged: ${!isLogged} wasDown: ${instanceStatus[usefulInstance] ?? true}`)
         requestJustLogs(response, requestedChannel, requestedLimit)
     }
 })
 
 
-function requestRecentMSG(response: any, requestedChannel: string, requestedLimit: number) {
-    const recentMessages = `${config.recentMsgInstance}/api/v2/recent-messages/${requestedChannel}?limit=${requestedLimit}`
+function requestRecentMSG(response: any, requestedChannel: string, requestedLimit: number, instance: string) {
+    const recentMessages = `${instance}/api/v2/recent-messages/${requestedChannel}?limit=${requestedLimit}`
 
     got(recentMessages, {throwHttpErrors: false}).then(result => {
         response.header('content-type', 'application/json')
@@ -72,11 +89,11 @@ function requestRecentMSG(response: any, requestedChannel: string, requestedLimi
             response.send(result.rawBody)
         }
     }).catch(() => {
-        lastRMDowntime = new Date()
         console.log('recent-messages request failed')
         response.sendStatus(500)
     })
 }
+
 
 function requestJustLogs(response: any, requestedChannel: string, requestedLimit: number) {
     got(`${config.recentMsgJustLogsInstance}/${requestedChannel}`).json<RecentMessages>().then(result => {
@@ -107,18 +124,21 @@ function convertIRCMessage(ircMsg: string) {
     return ircMsg.replace(regexInsertRMTags, `$1historical=1;$2rm-received-ts=${tmiTS};$3`)
 }
 
-function checkIsDown() {
-    got(`${config.recentMsgInstance}/api/v2/recent-messages/florian_2807?limit=1`).json<RecentMessages>().then(result => {
-        if (result.error !== null) {
-            console.error("recent-messages went down")
-            fs.writeFileSync('./last-down.json', new Date().toISOString())
-            lastRMDowntime = new Date()
-        }
-    }).catch(() => {
-        lastRMDowntime = new Date()
-        console.error("recent-messages went down" + lastRMDowntime)
-        fs.writeFileSync('./last-down.json', new Date().toISOString())
-    })
+function getAvailableRecentMSG() {
+    let lastDown = require('./last-down.json')
+    for (const instance of config.recentMsgInstance) {
+        got(`${instance}/api/v2/recent-messages/forsen?limit=1`).json<RecentMessages>().then(result => {
+            if (result.error !== null) {
+                console.error(`${instance} went down`)
+                lastDown[instance] = new Date().toISOString()
+                fs.writeFileSync('./last-down.json', JSON.stringify(lastDown, null, 4))
+            }
+        }).catch(() => {
+            console.error(`${instance} went down`)
+            lastDown[instance] = new Date().toISOString()
+            fs.writeFileSync('./last-down.json', JSON.stringify(lastDown, null, 4))
+        })
+    }
 }
 
 function getAvailableChannels() {
@@ -129,4 +149,28 @@ interface RecentMessages {
     error: string
     error_code: string
     messages: string[]
+}
+
+
+function checkCorrectConfig() {
+    if (!config.port || typeof config.port !== 'number') {
+        console.error('no port specified')
+        process.exit(1)
+    }
+    if (!config.recentMsgInstance || !Array.isArray(config.recentMsgInstance)) {
+        console.error('no recent-messages instance specified')
+        process.exit(1)
+    }
+    if (!config.recentMsgJustLogsInstance || typeof config.recentMsgJustLogsInstance !== 'string') {
+        console.error('no recent-messages instance specified')
+        process.exit(1)
+    }
+    if (!config.justlogsInstance || typeof config.justlogsInstance !== 'string') {
+        console.error('no justlogs instance specified')
+        process.exit(1)
+    }
+    if (!lastRecordedRMDowntime || typeof lastRecordedRMDowntime !== 'object') {
+        console.error('last-down.json not correct')
+        process.exit(1)
+    }
 }
